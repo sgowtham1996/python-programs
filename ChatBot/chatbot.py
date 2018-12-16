4ecefc828ae34c919a4d00f667770fe9bcf9eca5
Gowtham S <35688139+sgowtham1996@users.noreply.github.com>
2018-12-16 22:34:46 +0530

    Chartbot using chartbot-corpus module and os
    Chartbot using chartbot-corpus module and os, use a terminal for running, use chatbot train first and use chartbot.py for results it can give reply with 90% accuracy.

---
+++ ChatBot/chatbot.py
@@ -0,0 +1,21 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+import os
+bot = ChatBot('Bot')
+bot.set_trainer(ListTrainer)
+
+#for files in os.listdir('C:/Users/Gowtham\AppData\Local\Programs\Python\Python36-32\Lib\site-packages\chatterbot_corpus\data\english/'):
+    #data = open(C:/Users/Gowtham\AppData\Local\Programs\Python\Python36-32\Lib\site-packages\chatterbot_corpus\data\english/' + files,'r').readlines()
+    #bot.train(data)
+    #use the above commands for train purpose once  you have done training no need to use above commands
+
+while True:
+    message = input('You:')
+    if message.strip() != 'Bye':
+        reply = bot.get_response(message)
+        print('ChatBot:',reply)
+    
+    if message.strip() == 'Bye':
+        print('Chartbot: bye ')
+        break
+
